e9a2ab22300bef14d532a1a1cc3f74be9a60b191
AKASH KAMANE <akashkamane97@gmail.com>
2019-08-29 14:38:17 +0530

    @

--- src/main/java/com/demo/mvc/bean/dao/ManageEmployeeDaoImp.java
+++ src/main/java/com/demo/mvc/bean/dao/ManageEmployeeDaoImp.java
@@ -11,15 +11,19 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.demo.mvc.bean.Employee;
 
-
+@Repository
 public class ManageEmployeeDaoImp implements employeeDao {
+	@Autowired
 	private static SessionFactory sessionFactory;
 	public static void main(String[] args) {
 		try {
 	         //sessionFactory = new Configuration().configure().buildSessionFactory();
+			
 			 sessionFactory = new Configuration().configure().addAnnotatedClass(Employee.class).buildSessionFactory();
 	      } catch (Throwable ex) { 
 	         System.err.println("Failed to create sessionFactory object." + ex);
